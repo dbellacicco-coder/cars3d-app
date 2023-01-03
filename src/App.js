fc92336b1729229164e3a709a11f53f517ed997e
Daniel Bellacicco <dbellacicco@ideaingenieria.es>
2023-01-03 21:40:24 +0100

    Creating scene with three-fiber and adding orbitControls

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import logo from "./logo.svg";
+import React, { useRef } from "react";
 import "./App.css";
 import * as THREE from "three";
+import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+extend({ OrbitControls });
 
-function App() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+const Orbit = () => {
+  const { camera, gl } = useThree();
+  return <orbitControls args={[camera, gl.domElement]} />;
+};
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.innerHTML = "";
-  document.body.appendChild(renderer.domElement);
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  camera.position.z = 5;
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-  animate();
-
-  window.addEventListener("resize", () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+const BoxMesh = (props) => {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
   });
-
-  return null;
-}
+  return (
+    <mesh ref={ref} {...props}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
+};
+const App = () => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
+        <BoxMesh position={[1, 1, 0]} />
+        <Orbit />
+        <axesHelper args={[2]} />
+      </Canvas>
+    </div>
+  );
+};
 
 export default App;
